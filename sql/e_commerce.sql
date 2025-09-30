9637e47fbbcfb198754d024e354516654937e570
LiggMax <wenzhouli06@gmail.com>
2025-09-30 19:17:02 +0800

    feat(sql)

--- sql/e_commerce.sql
+++ sql/e_commerce.sql
@@ -26,7 +26,7 @@ CREATE TABLE `carousel` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL COMMENT '标题',
   `description` char(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL COMMENT '描述',
-  `status` int NOT NULL COMMENT '状态',
+  `status` tinyint NOT NULL COMMENT '状态',
   `target` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci NOT NULL COMMENT '打开方式',
   `sort` int NOT NULL COMMENT '排序',
   `subtitle` char(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_cs_0900_ai_ci DEFAULT NULL COMMENT '副标题',
@@ -36,7 +36,7 @@ CREATE TABLE `carousel` (
   `created_at` datetime NOT NULL COMMENT '创建时间',
   `update_at` datetime NOT NULL COMMENT '最后更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_cs_0900_ai_ci COMMENT='轮播图';
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_cs_0900_ai_ci COMMENT='轮播图';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `carousel` (
 
 LOCK TABLES `carousel` WRITE;
 /*!40000 ALTER TABLE `carousel` DISABLE KEYS */;
-INSERT INTO `carousel` VALUES (22,'qew','qwe',123,'qwe',123,'qw','/api/image/Carousel/2025-09-27/b2fb06a7-更衣人偶坠入爱河_第二季_1757496429774.jpg','qwe','qew','2025-09-27 15:05:51','2025-09-28 17:54:52'),(23,'qew','qwe',123,'qwe',123,'qw','/api/image/Carousel/2025-09-27/ffe4e7e1-胆大党_第二季_1757496400778.jpg','qwe','qew','2025-09-27 15:10:57','2025-09-28 17:54:53');
+INSERT INTO `carousel` VALUES (22,'132','qwe',1,'qwe',2,'qw','/api/image/Carousel/2025-09-29/a1273813-胆大党_第二季_1757496400778.jpg','qwe','qew','2025-09-27 15:05:51','2025-09-30 11:34:55'),(24,'达娃大大·','大王大大',0,'_self',1,'达娃大','/api/image/Carousel/2025-09-29/1eef2431-1756888554351.jpg','','了解更多','2025-09-29 16:25:22','2025-09-30 15:46:41');
 /*!40000 ALTER TABLE `carousel` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,7 +66,6 @@ CREATE TABLE `featured` (
   `rating` float NOT NULL DEFAULT '5' COMMENT '评价',
   `created_at` datetime NOT NULL COMMENT '上传时间',
   `update_at` datetime NOT NULL COMMENT '更新时间',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='特色商品';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -77,8 +76,58 @@ CREATE TABLE `featured` (
 
 LOCK TABLES `featured` WRITE;
 /*!40000 ALTER TABLE `featured` DISABLE KEYS */;
-INSERT INTO `featured` VALUES (1972196347546394625,'胆大党123123','/api/image/Featured/2025-09-28/f3372577-胆大党_第二季_1757496400778.jpg',100,80,0,0,'2025-09-28 15:07:06','2025-09-28 16:12:06','2025-09-28 16:46:56');
+INSERT INTO `featured` VALUES (1972871107687178241,'任务图我','/api/image/Featured/2025-09-30/2002b402-更衣人偶坠入爱河_第二季_1757496429774.jpg',123,12,0,9,'2025-09-30 11:48:21','2025-09-30 11:48:21'),(1972967206737379330,'title','/api/image/Featured/2025-09-30/c2b40d1f-更衣人偶坠入爱河_第二季_1757496429774.jpg',111,11,0,6,'2025-09-30 18:10:13','2025-09-30 18:10:13');
 /*!40000 ALTER TABLE `featured` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `featured_detail`
+--
+
+DROP TABLE IF EXISTS `featured_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `featured_detail` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `featured_id` bigint NOT NULL COMMENT '商品id',
+  `description` varchar(255) NOT NULL COMMENT '商品描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='精选商品详情';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `featured_detail`
+--
+
+LOCK TABLES `featured_detail` WRITE;
+/*!40000 ALTER TABLE `featured_detail` DISABLE KEYS */;
+INSERT INTO `featured_detail` VALUES (1,1972960556001492994,'daw'),(2,1972967206737379330,'描述');
+/*!40000 ALTER TABLE `featured_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_image`
+--
+
+DROP TABLE IF EXISTS `product_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_image` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` binary(1) NOT NULL COMMENT '商品id',
+  `sort` int DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL COMMENT '图片路径',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_image`
+--
+
+LOCK TABLES `product_image` WRITE;
+/*!40000 ALTER TABLE `product_image` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_image` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -120,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-28 18:16:02
+-- Dump completed on 2025-09-30 19:16:12
