c53787cd9abce1e4bf4908b66c84e32ece8d34e4
LiggMax <wemzhouli06@gmail.com>
2025-10-17 00:13:26 +0800

    feat(search): 完善商品搜索功能

--- common/src/main/java/com/ligg/common/module/vo/search/SearchVo.java
+++ common/src/main/java/com/ligg/common/module/vo/search/SearchVo.java
@@ -50,5 +50,4 @@ public class SearchVo {
      */
     @Schema(description = "折扣")
     private Double discount;
-
 }
--- web/api-client/src/main/java/com/ligg/apiclient/controller/SearchController.java
+++ web/api-client/src/main/java/com/ligg/apiclient/controller/SearchController.java
@@ -12,7 +12,6 @@ import com.ligg.common.utils.ImageUtil;
 import com.ligg.common.utils.Response;
 import com.ligg.common.module.vo.PageVo;
 import com.ligg.common.module.vo.search.SearchVo;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -35,5 +34,30 @@ public class SearchController {
 
     private final SearchService searchService;
 
-
+    /**
+     * 搜索商品
+     *
+     * @param keyword    关键字
+     * @param pageNumber 页码
+     * @param sort       排序(1: 默认排序 2:价格低到高 3:价格高到低 )
+     * @return 商品列表
+     */
+    @GetMapping
+    public Response<PageVo<SearchVo>> searchCommodity(@RequestParam @NotNull String keyword,
+                                                      @RequestParam(defaultValue = "1") Long pageNumber,
+                                                      @RequestParam(defaultValue = "1") Integer sort) {
+        PageVo<ProductEntity> pageVo = searchService.searchCommodityPageList(keyword, pageNumber, 20L, sort);
+        List<SearchVo> pageVolist = pageVo.getList().stream().map(product -> {
+            SearchVo searchVo = new SearchVo();
+            BeanUtils.copyProperties(product, searchVo);
+            searchVo.setUrl(ImageUtil.getImagePath(product.getImagePath()));
+            searchVo.setDiscount(DiscountUtil.calculateDiscountPercentage(product.getOriginalPrice(), product.getCurrentPrice()).doubleValue());
+            return searchVo;
+        }).toList();
+        PageVo<SearchVo> searchData = new PageVo<>();
+        searchData.setPages(pageVo.getPages());
+        searchData.setTotal(pageVo.getTotal());
+        searchData.setList(pageVolist);
+        return Response.success(BusinessStates.SUCCESS, searchData);
+    }
 }
