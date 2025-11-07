777bce0cd17fb435226b2b921c745ec7eb38b087
LiggMax <wenzhouli06@gmail.com>
2025-11-07 11:51:51 +0800

    feat(common): 更新搜索排序枚举值
    Signed-off-by: LiggMax <wenzhouli06@gmail.com>

--- common/src/main/java/com/ligg/common/enums/SearchSorting.java
+++ common/src/main/java/com/ligg/common/enums/SearchSorting.java
@@ -11,10 +11,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SearchSorting {
-    //价格排序
-    PRICE_ASC("价格升序"),
-    RATING_ASC("评分升序"),
-    HOT_ASC("热度升序");
+    DEFAULT("默认排序"),
+    PRICE("价格排序"),
+    RATING("评分排序"),
+    HOT("热度排序");
 
     private final String value;
 }
